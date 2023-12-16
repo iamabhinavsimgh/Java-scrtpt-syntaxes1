f842a3595b05a7b72d1f534bee18c108b0bf708f
iamabhinavsimgh <142678907+iamabhinavsimgh@users.noreply.github.com>
2023-12-16 20:44:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log("this is console screen")
+var fname="ABHINAV "
+var lname="singh"
+console.log(fname)
+console.log(lname)
+
+var fullname=fname.concat(lname)
+console.log(fullname)
+
+var  l1=fname.length
+var  l2=lname.length
+var  l3=fullname.length
+console.log(l1+" "+l2+" "+l3)
+
+var sentence="I am abdul" 
+var newSentence=sentence.replace("abdul","Abhinav")
+console.log(sentence)
+console.log(newSentence)
+
+var  fname1=fname.toLowerCase()
+console.log(fname1)
+
+var lname1=lname.toUpperCase()
+console.log(lname1)
+
+var minnumber=Math.min(1,3,4,5)
+console.log(minnumber)
+
+var power=Math.pow(3,2)
+console.log(power)
